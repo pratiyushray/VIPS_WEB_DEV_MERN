0c4e90657d80df37381761c7451a67a80e22ec2f
pratiyush ray <pkray0021@gmail.com>
2025-06-27 09:24:01 +0530

    Lec1: Intro to JS

--- L1_INTRO_TO_JS/script.js
+++ L1_INTRO_TO_JS/script.js
@@ -151,9 +151,9 @@
 // },2000);
 
 
-for (let i =0; i<=5;i++){
-    setTimeout(function (){
-        console.log(i)
-    },2000);
-}
+// for (let i =0; i<=5;i++){
+//     setTimeout(function (){
+//         console.log(i)
+//     },2000);
+// }
 
